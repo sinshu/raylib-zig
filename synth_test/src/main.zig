075901de3b343c995875b386da29ee422a8634e9
田中信秋 <sinshu.d@gmail.com>
2023-01-28 12:36:29 +0900

    Update synth.

--- synth_test/src/main.zig
+++ synth_test/src/main.zig
@@ -11,16 +11,25 @@ const Synthesizer = ziggysynth.Synthesizer;
 const SynthesizerSettings = ziggysynth.SynthesizerSettings;
 const MidiFile = ziggysynth.MidiFile;
 const MidiFileSequencer = ziggysynth.MidiFileSequencer;
+const CN = @import("./fft.zig").CN;
+const fft = @import("./fft.zig").fft;
+const ifft = @import("./fft.zig").ifft;
 
 const sample_rate = 44100;
 const buffer_size = 2048;
+const screen_width = 1600;
+const screen_height = 800;
 
 pub fn main() anyerror!void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer debug.assert(!gpa.deinit());
 
-    rl.InitWindow(800, 450, "MIDI Player");
+    var fft_in = mem.zeroes([buffer_size]CN);
+    var fft_out = mem.zeroes([buffer_size]CN);
+    var smoothed = mem.zeroes([buffer_size]f32);
+
+    rl.InitWindow(screen_width, screen_height, "MIDI Player");
 
     rl.InitAudioDevice();
     rl.SetAudioStreamBufferSizeDefault(buffer_size);
@@ -56,41 +65,13 @@ pub fn main() anyerror!void {
     // Play the MIDI file.
     sequencer.play(midi_file, true);
 
-    var speed_x10: i32 = 10;
-    var speed_str_buf: [64]u8 = undefined;
-
     rl.SetTargetFPS(60);
 
-    var left_level: f32 = 0.0;
-    var right_level: f32 = 0.0;
-
     while (!rl.WindowShouldClose()) {
-
-        if (rl.IsKeyPressed(rl.KeyboardKey.KEY_LEFT)){
-            speed_x10 -= 1;
-            if (speed_x10 < 1) {
-                speed_x10 = 1;
-            }
-        }
-
-        if (rl.IsKeyPressed(rl.KeyboardKey.KEY_RIGHT)){
-            speed_x10 += 1;
-            if (speed_x10 > 100) {
-                speed_x10 = 100;
-            }
-        }
-
-        const speed = @intToFloat(f64, speed_x10) / 10.0;
-        speed_str_buf = mem.zeroes([64]u8);
-        const speed_str_slice = try std.fmt.bufPrint(&speed_str_buf, "x{d:.1}", .{speed});
-
-        sequencer.speed = speed;
 
         if (rl.IsAudioStreamProcessed(stream)) {
             sequencer.render(&left, &right);
             var t: usize = 0;
-            var left_max: f32 = 0.0;
-            var right_max: f32 = 0.0;
             while (t < buffer_size) : (t += 1) {
                 var left_sample_i32: i32 = @floatToInt(i32, 32768.0 * left[t]);
                 if (left_sample_i32 < -32768) {
@@ -99,9 +80,6 @@ pub fn main() anyerror!void {
                 if (left_sample_i32 > 32767) {
                     left_sample_i32 = 32767;
                 }
-                if (left[t] > left_max) {
-                    left_max = left[t];
-                }
                 var right_sample_i32: i32 = @floatToInt(i32, 32768.0 * right[t]);
                 if (right_sample_i32 < -32768) {
                     right_sample_i32 = -32768;
@@ -109,26 +87,51 @@ pub fn main() anyerror!void {
                 if (right_sample_i32 > 32767) {
                     right_sample_i32 = 32767;
                 }
-                if (right[t] > right_max) {
-                    right_max = right[t];
-                }
                 var left_sample_i16 = @truncate(i16, left_sample_i32);
                 var right_sample_i16 = @truncate(i16, right_sample_i32);
                 buffer[2 * t] = left_sample_i16;
                 buffer[2 * t + 1] = right_sample_i16;
+
+                fft_in[t].re = 0.5 * (left[t] + right[t]);
+                fft_in[t].im = 0.0;
             }
             rl.UpdateAudioStream(stream, &buffer, buffer_size);
-            left_level = left_max;
-            right_level = right_max;
         }
 
+        fft(buffer_size, &fft_in, &fft_out);
+
+        var backColor = rl.Color{
+            .r = 0x00, .g = 0x69, .b = 0x5C, .a = 0xFF
+        };
+
+        var barColor = rl.Color{
+            .r = 0x00, .g = 0x96, .b = 0x88, .a = 0xFF
+        };
+
+        var textColor = rl.Color{
+            .r = 0xB2, .g = 0xDF, .b = 0xDB, .a = 0xFF
+        };
+
         rl.BeginDrawing();
-        rl.ClearBackground(rl.LIGHTGRAY);
-        rl.DrawText("Playback", 140, 155, 50, rl.DARKGRAY);
-        rl.DrawText("speed", 140, 205, 50, rl.DARKGRAY);
-        rl.DrawText(@ptrCast(*const u8, speed_str_slice), 400, 130, 150, rl.DARKGRAY);
-        rl.DrawRectangle(140, 270, @floatToInt(i32, 600.0 * left_level), 15, rl.GRAY);
-        rl.DrawRectangle(140, 285, @floatToInt(i32, 600.0 * right_level), 15, rl.GRAY);
+        rl.ClearBackground(backColor);
+        rl.DrawText("MIDI music playback", 750, 150, 75, textColor);
+        rl.DrawText("with raylib-zig", 900, 250, 75, textColor);
+
+        {
+            var t: usize = 0;
+            const lim = screen_width / 4;
+            while (t < lim) : (t += 1) {
+                const c = fft_out[t];
+                const val = @floatCast(f32, 100 * @max(@log10(c.re * c.re + c.im * c.im) + 1.5, 0.0));
+                if (val > smoothed[t]) {
+                    smoothed[t] = 0.5 * smoothed[t] + 0.5 * val;
+                } else {
+                    smoothed[t] = 0.95 * smoothed[t] + 0.05 * val;
+                }
+                const top = @intToFloat(f32, screen_height) - smoothed[t];
+                rl.DrawRectangle(@intCast(c_int, 4 * t), @floatToInt(i32, top), 2, @floatToInt(i32, smoothed[t]) + 2, barColor);
+            }
+        }
         rl.EndDrawing();
     }
 
